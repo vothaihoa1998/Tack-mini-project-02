fccfe497aba8fdd8646d41901225482a4e771b34
Võ Thái Hòa <thaihoavo1998@gmail.com>
2020-12-09 17:32:19 +0700

    create header

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,21 @@ $('.dropdown').on('click', function(e) {
     var dropdown = target.closest('.dropdown');
     return !dropdown.hasClass('open') || !target.hasClass('dropdown-menu__search__input');
 });
+
+$('.search-button').click(function(){
+    $(this).parent().toggleClass('open');
+  });
+
+  $(document).ready(function(){
+    $('.banner-slick').slick({
+        mobileFirst: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        pauseOnDotsHover:true,
+        autoplaySpeed:1000,
+        dots: true,
+        arrows: false,
+        infinite: true
+    });
+});
